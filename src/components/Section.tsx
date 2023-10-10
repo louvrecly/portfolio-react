df97693ec66690d0f3ac57eb7ef0e094153da7d0
Louvre Chow <louvrecly@gmail.com>
2023-10-10 12:38:47 -0400

    chore/styling: fix padding in the Section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -12,7 +12,6 @@ const Section = ({ title = '', id, children }: SectionProps) => {
       id={id}
       className="tw-relative tw-min-h-screen tw-flex tw-flex-col tw-scroll-m-16"
     >
-      <h1></h1>
       {title && (
         <h2 className="tw-p-5 tw-text-3xl tw-font-bold tw-flex tw-items-center tw-gap-4">
           <span className="tw-border-y-2 tw-border-inherit tw-h-0 tw-flex-1"></span>
@@ -21,7 +20,7 @@ const Section = ({ title = '', id, children }: SectionProps) => {
         </h2>
       )}
 
-      <div className="tw-pb-16 tw-px-5 tw-flex-1 tw-flex tw-items-center tw-justify-center">
+      <div className={'tw-pb-16 tw-px-5 tw-flex-1 tw-flex tw-items-center tw-justify-center' + (!title ? ' tw-pt-16' : '')}>
         {children}
       </div>
     </section>
