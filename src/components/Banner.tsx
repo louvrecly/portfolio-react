e36dafe03d70b3e84fd7a399181b8db7e4c031c5
Louvre Chow <louvrecly@gmail.com>
2023-10-13 01:25:02 -0400

    feat/background-styling: add GlowBackground to Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,5 @@
 import Section from './Section';
+import GlowBackground from './GlowBackground';
 
 interface BannerProps {
   id?: string;
@@ -10,6 +11,8 @@ interface BannerProps {
 const Banner = ({ id, name, title, location }: BannerProps) => {
   return (
     <Section id={id}>
+      <GlowBackground />
+
       <div className="u-absolute u-inset-0 u-bg-gradient-to-t u-from-zinc-950 u-to-transparent u-opacity-50"></div>
 
       <div className="u-relative">
