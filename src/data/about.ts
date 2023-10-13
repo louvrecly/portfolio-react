8202b96e1792278d406c37471d131cd32bcf3780
Louvre Chow <louvrecly@gmail.com>
2023-10-13 14:35:19 -0400

    chore/about-data: update about data

--- src/data/about.ts
+++ src/data/about.ts
@@ -8,14 +8,14 @@ interface AboutData {
 export const aboutData: AboutData = {
   name: 'Louvre Chow',
   description:
-    'As a driven Full Stack Software Engineer with 4 years of experience, I am passionate in creating frontend UIs, developing backend servers with RESTful APIs, configuring both relational and NoSQL databases, building productivity tools and making architectural decisions on the software design components. I am also a hackathon winner, puzzle solver, knowledge sharer and active learner. I am looking for an opportunity to utilize my expertise in software engineering to craft products with a fully tested codebase and sustainable development.',
+    'As a driven Full Stack Software Engineer with 4 years of experience, I am passionate about creating frontend UIs, developing backend servers with RESTful APIs, configuring both relational and NoSQL databases, building productivity tools, and making architectural decisions on the software design components. I am also a hackathon winner, puzzle solver, knowledge sharer, and active learner.',
   identities: [
-    'Full Stack Software Engineer',
-    'Hackathon Winner',
-    'Puzzle Solver',
-    'STEM Instructor',
-    'Knowledge Sharer',
-    'Active Learner',
+    'Software Engineer 💻',
+    'Data Analyst 📊',
+    'Knowledge Sharer 🎓',
+    'Active Learner 📚',
+    'Hackathon Winner 🏆',
+    'Puzzle Solver 💡',
   ],
   resumeLink: '#',
 };
