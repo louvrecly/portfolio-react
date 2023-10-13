28e9e0f9c9a2d9a4ffed83cae0f5000b8cd49908
Louvre Chow <louvrecly@gmail.com>
2023-10-13 01:24:36 -0400

    feat/background-styling: create GlowBackground

---
+++ src/components/GlowBackground.tsx
@@ -0,0 +1,26 @@
+import '../styles/GlowBackground.scss';
+
+const GlowBackground = () => {
+  const circles = new Array(100).fill(null);
+
+  return (
+    <div className="u-absolute u-inset-0 u-overflow-hidden o-glow-background">
+      {circles.map((_, index) => (
+        <div
+          key={index}
+          className={`o-glow-background__particle-container o-glow-background__particle-container--${
+            index + 1
+          }`}
+        >
+          <div
+            className={`o-glow-background__particle o-glow-background__particle--${
+              index + 1
+            }`}
+          ></div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GlowBackground;
