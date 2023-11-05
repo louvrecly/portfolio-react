69b9dda245fba6b6cf3b4210d75c6091f39485e7
Louvre Chow <louvrecly@gmail.com>
2023-11-05 16:10:14 -0500

    chore/refactor: migrate GradientBackground to pure tailwind css

--- src/components/Background/GradientBackground.tsx
+++ src/components/Background/GradientBackground.tsx
@@ -1,10 +1,8 @@
-import '../../styles/GradientBackground.css';
-
 const GradientBackground = () => {
   return (
-    <div className="u-fixed u-inset-x-0 u-top-0 u-h-screen u-justify-center u-flex u-items-center o-gradient-background">
-      <div className="u-absolute o-gradient-background__glow o-gradient-background__glow--cool"></div>
-      <div className="u-absolute o-gradient-background__glow o-gradient-background__glow--warm"></div>
+    <div className="u-fixed u-inset-x-0 u-top-0 u-h-screen u-justify-center u-flex u-items-center u-animate-spin-slow">
+      <div className="u-absolute u-w-[50vh] u-h-[50vh] u-bg-gradient-to-r u-from-cool-positive u-to-cool-negative u-rounded-full u-blur-3xl u-animate-pulse-full"></div>
+      <div className="u-absolute u-w-[50vh] u-h-[50vh] u-bg-gradient-to-r u-from-warm-positive u-to-warm-negative u-rounded-full u-blur-3xl u-animate-pulse-full-alt"></div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,33 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+        'pulse-full': 'pulse-full 10s ease infinite',
+        'pulse-full-alt': 'pulse-full-alt 10s ease infinite',
+      },
+      colors: {
+        cool: {
+          positive: 'hsl(var(--color-cool-positive))',
+          negative: 'hsl(var(--color-cool-negative))',
+        },
+        warm: {
+          positive: 'hsl(var(--color-warm-positive))',
+          negative: 'hsl(var(--color-warm-negative))',
+        },
+      },
       fontFamily: {
         jost: ['Jost', 'sans-serif'],
+      },
+      keyframes: {
+        'pulse-full': {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+        'pulse-full-alt': {
+          '0%, 100%': { opacity: 0 },
+          '50%': { opacity: 1 },
+        },
       },
     },
   },
